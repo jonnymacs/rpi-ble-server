0ff23c038b6c13f498008f0d7baa39cdc775e858
Jonathan McAllister <jmcallis@gmail.com>
2025-04-01 22:16:18 -0700

    update build script to be more resilient to failures and ensure containers are cleaned up properly

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+BUILD_ID=${RANDOM}
 BINARY_BUILD_SVC="ble_server"
 BINARY_NAME="ble_server"
 RPI_BUILD_SVC="macmind_rpi_ble_server"
@@ -9,25 +10,45 @@ RPI_BUILD_USER="imagegen"
 RPI_CUSTOMIZATIONS_DIR="macmind_rpi_ble_server"
 RPI_IMAGE_NAME="macmind_rpi_ble_server"
 
+ensure_cleanup() {
+  echo "Cleanup containers..."
+
+  RPI_BUILD_SVC_CONTAINER_ID=$(docker ps -a --filter "name=${RPI_BUILD_SVC}-${BUILD_ID}" --format "{{.ID}}" | head -n 1) \
+    && docker kill ${RPI_BUILD_SVC_CONTAINER_ID} \
+    && docker rm ${RPI_BUILD_SVC_CONTAINER_ID}
+
+  BINARY_BUILD_SVC_CONTAINER_ID=$(docker ps -a --filter "name=${BINARY_BUILD_SVC}-${BUILD_ID}" --format "{{.ID}}" | head -n 1) \
+    && docker kill ${BINARY_BUILD_SVC_CONTAINER_ID} \
+    && docker rm ${BINARY_BUILD_SVC_CONTAINER_ID}
+
+  echo "Cleanup complete."
+}
+
+# Set the trap to execute the ensure_cleanup function on EXIT
+trap ensure_cleanup EXIT
+
 # build the ble server binary
 # and copy it to the imagegen_ext_dir
 # for the raspberry pi image build
 #
+echo "🔨 Building Docker image to compile ${BINARY_BUILD_SVC}..."
 docker compose build ${BINARY_BUILD_SVC}
 
-docker compose run -d ${BINARY_BUILD_SVC} \
+docker compose run --name ${BINARY_BUILD_SVC}-${BUILD_ID} -d ${BINARY_BUILD_SVC} \
   && docker compose exec ${BINARY_BUILD_SVC} bash -c "cargo build --release --target aarch64-unknown-linux-gnu" \
-  && docker ps -a --format '{{.Names}}' | grep ${BINARY_BUILD_SVC} | xargs -I '{}' -- docker cp {}:/app/target/aarch64-unknown-linux-gnu/release/${BINARY_NAME} ./${RPI_CUSTOMIZATIONS_DIR}/ext_dir/image/mbr/simple_dual/device/rootfs-overlay/usr/local/bin/${BINARY_NAME} \
-  && docker compose kill ${BINARY_BUILD_SVC} \
-  && docker ps -a --format '{{.Names}}' | grep ${BINARY_BUILD_SVC} | xargs -I '{}' -- docker rm {}
+  && CID=$(docker ps -a --filter "name=${BINARY_BUILD_SVC}-${BUILD_ID}" --format "{{.ID}}" | head -n 1) \
+  && docker cp ${CID}:/app/target/aarch64-unknown-linux-gnu/release/${BINARY_NAME} ./${RPI_CUSTOMIZATIONS_DIR}/ext_dir/image/mbr/simple_dual/device/rootfs-overlay/usr/local/bin/${BINARY_NAME}
 
 # Build a customer raspberry pi image
 # with the ble server included
 #
+echo "🔨 Building Docker image with rpi-image-gen to create ${RPI_BUILD_SVC}..."
 docker compose build ${RPI_BUILD_SVC}
 
-docker compose run -d ${RPI_BUILD_SVC} \
+echo "🚀 Running image generation in container..."
+docker compose run --name ${RPI_BUILD_SVC}-${BUILD_ID} -d ${RPI_BUILD_SVC} \
   && docker compose exec ${RPI_BUILD_SVC} bash -c "/home/${RPI_BUILD_USER}/rpi-image-gen/build.sh -D /home/${RPI_BUILD_USER}/${RPI_CUSTOMIZATIONS_DIR}/ext_dir -c ${RPI_IMAGE_NAME} -o /home/${RPI_BUILD_USER}/${RPI_CUSTOMIZATIONS_DIR}/ext_dir/${RPI_IMAGE_NAME}.options" \
-  && docker ps -a --format '{{.Names}}' | grep ${RPI_BUILD_SVC} | xargs -I '{}' -- docker cp {}:/home/${RPI_BUILD_USER}/rpi-image-gen/work/${RPI_IMAGE_NAME}/deploy/${RPI_IMAGE_NAME}.img .${RPI_CUSTOMIZATIONS_DIR}/deploy/${RPI_IMAGE_NAME}-$(date +%m-%d-%Y-%H%M).img \
-  && docker compose kill ${RPI_BUILD_SVC} \
-  && docker ps -a --format '{{.Names}}' | grep ${RPI_BUILD_SVC} | xargs -I '{}' -- docker rm {}
+  && CID=$(docker ps -a --filter "name=${RPI_BUILD_SVC}-${BUILD_ID}" --format "{{.ID}}" | head -n 1) \
+  && docker cp ${CID}:/home/${RPI_BUILD_USER}/rpi-image-gen/work/${RPI_IMAGE_NAME}/deploy/${RPI_IMAGE_NAME}.img ./${RPI_CUSTOMIZATIONS_DIR}/deploy/${RPI_IMAGE_NAME}-$(date +%m-%d-%Y-%H%M).img
+
+echo "🚀 Completed -> ${RPI_CUSTOMIZATIONS_DIR}/deploy/${RPI_IMAGE_NAME}-$(date +%m-%d-%Y-%H%M).img"
